be51d8037beb35bbf30cb12715364a228020e2ab
Bruno Sanches <bruno_sanches15@hotmail.com>
2024-01-12 17:39:48 -0300

    feat: implement board with tossed cards

---
+++ Uno/Public/Game/Match/Board.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <memory>
+#include <vector>
+
+class Card;
+
+class Board
+{
+public:
+
+    void Stack(std::shared_ptr<Card>&& Card);
+    const Card& PeekCurrentCard() const;
+    
+private:
+    std::vector<std::shared_ptr<Card>> TossedCards{};
+};
