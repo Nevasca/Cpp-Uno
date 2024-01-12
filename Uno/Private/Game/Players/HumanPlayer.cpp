c1f40460f2a9acd7f28517002267766c20eb73ff
Bruno Sanches <bruno_sanches15@hotmail.com>
2024-01-12 15:09:38 -0300

    feat: implement player cards

---
+++ Uno/Private/Game/Players/HumanPlayer.cpp
@@ -0,0 +1,12 @@
+﻿#include "Public/Game/Players/HumanPlayer.h"
+
+#include <iostream>
+
+HumanPlayer::HumanPlayer(std::string&& InName)
+    : Player(std::move(InName))
+{ }
+
+void HumanPlayer::PlayTurn()
+{
+    std::cout << Name << " has chosen to just watch...";
+}
