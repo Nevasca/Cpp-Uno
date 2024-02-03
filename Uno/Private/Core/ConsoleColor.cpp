9d9daca092c08b8d7f5bb9d499003302f30deef7
Bruno Sanches <bruno_sanches15@hotmail.com>
2024-02-02 23:34:42 -0300

    feat: print cards with color

---
+++ Uno/Private/Core/ConsoleColor.cpp
@@ -0,0 +1,30 @@
+﻿#include "Public/Core/ConsoleColor.h"
+
+#include "Windows.h"
+
+const ConsoleColor ConsoleColor::White = {7};
+const ConsoleColor ConsoleColor::Blue = {FOREGROUND_BLUE};
+const ConsoleColor ConsoleColor::Green = {FOREGROUND_GREEN};
+const ConsoleColor ConsoleColor::Red = {FOREGROUND_RED};
+const ConsoleColor ConsoleColor::Yellow = {FOREGROUND_RED | FOREGROUND_GREEN};
+
+ConsoleColor::ConsoleColor(uint16_t InColorFlag)
+    : Color(InColorFlag)
+{ }
+
+void ConsoleColor::SetFontColor(uint16_t ColorFlag)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ColorFlag);
+}
+
+void ConsoleColor::SetFontColor(ConsoleColor Color)
+{
+    SetFontColor(Color.Color);
+}
+
+std::ostream& operator<<(std::ostream& Os, const ConsoleColor& ConsoleColor)
+{
+    ConsoleColor::SetFontColor(ConsoleColor.Color);
+    
+    return Os;
+}
--- Uno/Public/Game/UI/CardConsolePresenter.h
+++ Uno/Public/Game/UI/CardConsolePresenter.h
@@ -1,8 +1,11 @@
 ﻿#pragma once
+#include <map>
 #include <memory>
 #include <vector>
 
 #include "CardRenderData.h"
+#include "Public/Core/ConsoleColor.h"
+#include "Public/Game/EColor.h"
 
 class Card;
 
@@ -10,7 +13,12 @@ class CardConsolePresenter
 {
 public:
 
+    CardConsolePresenter();
     void Show(const Card& Card);
     void Show(const std::vector<std::shared_ptr<Card>>& Cards);
     void Show(const std::vector<CardRenderData>& CardsData);
+
+private:
+
+    std::map<EColor, ConsoleColor> ColorMapping{};
 };
